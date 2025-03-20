941911bc8638f39a69bebcf271e2c4e49196ed7a
LiuShihao <liush99@foxmail.com>
2025-03-20 16:08:00 +0800

    update

--- src/agent/graph.py
+++ src/agent/graph.py
@@ -1,5 +1,8 @@
+import os
 from datetime import datetime, timezone
 from typing import Dict, List, Literal, cast
+
+from langchain_community.chat_models import ChatTongyi
 
 from agent.configuration import Configuration
 from langchain_core.messages import AIMessage
@@ -7,18 +10,18 @@ from langchain_core.runnables import RunnableConfig
 from langgraph.graph import StateGraph
 from langchain_deepseek import ChatDeepSeek
 from langgraph.checkpoint.memory import MemorySaver
-from langgraph.prebuilt import ToolNode, tools_condition
+from langgraph.prebuilt import ToolNode
 
 from agent.state import State, InputState
-from agent.tools import human_assistance, milvus_search, amultiply
+from agent.tools import TOOLS
 
-# tools = [human_assistance, milvus_search, amultiply]
-tools = [milvus_search, amultiply]
+os.environ["DASHSCOPE_API_KEY"] = "..."
+"""
+pip install --upgrade --quiet  dashscope
+"""
 
 
-async def call_model(
-        state: State, config: RunnableConfig
-) -> Dict[str, List[AIMessage]]:
+async def call_model(state: State, config: RunnableConfig) -> Dict[str, List[AIMessage]]:
     """Call the LLM powering our "agent".
 
     This function prepares the prompt, initializes the model, and processes the response.
@@ -32,17 +35,19 @@ async def call_model(
     """
     configuration = Configuration.from_runnable_config(config)
 
+    # model = ChatDeepSeek(
+    #     model=configuration.model,
+    #     temperature=0,
+    #     max_tokens=None,
+    #     timeout=None,
+    #     max_retries=2,
+    # ).bind_tools(TOOLS)
+
+    model = ChatTongyi().bind_tools(TOOLS)
+
     system_message = configuration.system_prompt.format(
         system_time=datetime.now(tz=timezone.utc).isoformat()
     )
-
-    model = ChatDeepSeek(
-        model="deepseek-chat",
-        temperature=0,
-        max_tokens=None,
-        timeout=None,
-        max_retries=2,
-    ).bind_tools(tools)
 
     # Get the model's response
     response = cast(
@@ -63,6 +68,14 @@ async def call_model(
 
     # Return the model's response as a list to be added to existing messages
     return {"messages": [response]}
+
+
+graph_builder = StateGraph(State, input=InputState, config_schema=Configuration)
+
+graph_builder.add_node("call_model", call_model)
+graph_builder.add_node("tools", ToolNode(TOOLS))
+
+graph_builder.add_edge("__start__", "call_model")
 
 
 def route_model_output(state: State) -> Literal["__end__", "tools"]:
@@ -88,13 +101,6 @@ def route_model_output(state: State) -> Literal["__end__", "tools"]:
     return "tools"
 
 
-graph_builder = StateGraph(State, input=InputState, config_schema=Configuration)
-
-graph_builder.add_node("call_model", call_model)
-
-graph_builder.add_node("tools", ToolNode(tools=tools))
-graph_builder.add_edge("__start__", "call_model")
-
 graph_builder.add_conditional_edges(
     "call_model",
     route_model_output,
@@ -102,14 +108,14 @@ graph_builder.add_conditional_edges(
 # Any time a tool is called, we return to the chatbot to decide the next step
 graph_builder.add_edge("tools", "call_model")
 
-memory = MemorySaver()
+# memory = MemorySaver()
 
 # Compile the builder into an executable graph
 # You can customize this by adding interrupt points for state updates
 graph = graph_builder.compile(
     # interrupt_before=[],  # Add node names here to update state before they're called
     # interrupt_after=[],  # Add node names here to update state after they're called
-    checkpointer=memory,
+    # checkpointer=memory,
 )
 
 graph.name = "New Graph"  # This defines the custom name in LangSmith

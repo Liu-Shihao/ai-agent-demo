09b2e5496fbe4452ccc1d6f57c7be108e6371c44
LiuShihao <liush99@foxmail.com>
2025-03-31 21:30:17 +0800

    update

--- src/llm/deepseek_api.py
+++ src/llm/deepseek_api.py
@@ -5,6 +5,7 @@ import requests
 
 os.environ["DEEPSEEK_API_KEY"] = "..."
 
+
 def model_list():
     url = "https://api.deepseek.com/models"
     payload = {}
@@ -18,5 +19,33 @@ def model_list():
     print(response.text)
 
 
+def reasoner_test():
+    from openai import OpenAI
+    client = OpenAI(api_key="...", base_url="https://api.deepseek.com")
+
+    # Round 1
+    messages = [{"role": "user", "content": "9.11 and 9.8, which is greater?"}]
+    response = client.chat.completions.create(
+        model="deepseek-reasoner",
+        messages=messages
+    )
+
+    reasoning_content = response.choices[0].message.reasoning_content
+    print(reasoning_content)
+    content = response.choices[0].message.content
+    print(content)
+
+    # Round 2
+    # messages.append({'role': 'assistant', 'content': content})
+    # messages.append({'role': 'user', 'content': "How many Rs are there in the word 'strawberry'?"})
+    # response = client.chat.completions.create(
+    #     model="deepseek-reasoner",
+    #     messages=messages
+    # )
+    # print(response)
+    # ...
+
+
 if __name__ == "__main__":
-    model_list()
+    # model_list()
+    reasoner_test()
